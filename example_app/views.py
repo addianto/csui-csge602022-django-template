2ad769b52f7f087c228ec3938ee200c7ef2c7889
Daya Adianto <dayaadianto@cs.ui.ac.id>
2022-09-04 19:28:32 +0700

    Spike basic greeting functionality

--- example_app/views.py
+++ example_app/views.py
@@ -1,4 +1,14 @@
+from django.http.request import HttpRequest, QueryDict
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def hello(request: HttpRequest):
+    payload: QueryDict = request.GET
+
+    if 'name' in payload:
+        return HttpResponse(payload['name'])
+    else:
+        return HttpResponse('anonymous')
