713e1e05f83e5a1ddd4872d34930bd0c5cae28da
Daya Adianto <dayaadianto@cs.ui.ac.id>
2022-09-04 19:44:19 +0700

    Stub tests for controller (views)

--- example_app/tests.py
+++ example_app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class ViewsTestCase(TestCase):
+
+    def test_hello_with_empty_request(self):
+        pass
+
+    def test_hello_with_valid_request(self):
+        pass
+    
+    def test_hello_with_invalid_request(self):
+        pass
