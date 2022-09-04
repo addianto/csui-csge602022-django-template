d3616f846a05c4c4ade55bd3165331ae79258fee
Daya Adianto <dayaadianto@cs.ui.ac.id>
2022-09-04 20:11:27 +0700

    Add tests for views.py in the sample app

--- example_app/tests.py
+++ example_app/tests.py
@@ -1,13 +1,41 @@
-from django.test import TestCase
+from django.http.request import HttpRequest
+from django.test import Client, TestCase
+from .views import TITLES, hello
+from functools import reduce
 
 
 class ViewsTestCase(TestCase):
 
+    def setUp(self):
+        self.client = Client()
+
     def test_hello_with_empty_request(self):
-        pass
+        response = hello(HttpRequest())
+
+        self.assertEquals(200, response.status_code)
+        self.assertTrue(is_title_exist(str(response.content), TITLES))
 
     def test_hello_with_valid_request(self):
-        pass
+        request = HttpRequest()
+        request.GET['name'] = 'Alice'
+
+        response = hello(request)
+
+        self.assertContains(response, status_code=200, text='Alice')
     
     def test_hello_with_invalid_request(self):
-        pass
+        request = HttpRequest()
+        request.GET['stand'] = 'Star Platinum'
+
+        response = hello(request)
+
+        self.assertNotEquals(200, response.status_code)
+        self.assertEquals(400, response.status_code)
+    
+
+def is_title_exist(content, titles):
+    for title in titles:
+        if title in content:
+            return True
+    
+    return False
